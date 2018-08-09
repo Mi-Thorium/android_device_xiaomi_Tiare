e324311286f0c45c23eec691d7bbc0f155fbd109
Shibin George <shibing@codeaurora.org>
2018-08-09 10:32:29 +0530

    Tiare: Go: Do not uncompress dex files in priv-app apks
    Android P doesn't compress dex files in priv-apps by default.
    This can add up to a non-trivial amount on system partition.
    For Go targets, where disk space is a concern, compress
    the classes*.dex in these apks.
    
    BUG: 112348966
    
    Change-Id: I748b4497ca7f4e630187655277ec3358b5e85fd6

--- device.mk
+++ device.mk
@@ -45,6 +45,9 @@ PRODUCT_PACKAGES += \
 # Enable DM file preopting to reduce first boot time
 PRODUCT_DEX_PREOPT_GENERATE_DM_FILES := true
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
+# On P, classes*.dex of privileged apps are left uncompressed by default
+# For Go targets, compress them to save disk space.
+DONT_UNCOMPRESS_PRIV_APPS_DEXS := true
 
 # Input
 PRODUCT_COPY_FILES += \
