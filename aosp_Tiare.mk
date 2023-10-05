09413f68e598b4e5f9a0e9a0ffdd52f744ee4c06
me-cafebabe <me.cafebabe@gmail.com>
2023-10-05 16:46:37 +0800

    Tiare: Set ro.build.fingerprint
    If we didn't set this, init's property_derive_build_fingerprint() function would generate a build fingerprint and store to the prop

--- aosp_Tiare.mk
+++ aosp_Tiare.mk
@@ -30,5 +30,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "Xiaomi/tiare/tiare:8.1.0/OPM1.171019.026/V10.2.25.0.OCLMIXM:user/release-keys"
 
+PRODUCT_SYSTEM_PROPERTIES += ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
 # Extra
 EXTRA_LITE := true
